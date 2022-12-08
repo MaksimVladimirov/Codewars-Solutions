ce92ddcd381e14d291538a5de4466f39430f9325
Maksim Vladimirov <vl4dimirov@gmail.com>
2022-12-08 11:09:16 +0300

    new solution

---
+++ String ends with?.js
@@ -0,0 +1,25 @@
+// Description:
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+// Tests:
+// const Test = require('@codewars/test-compat');
+// describe("Tests", () => {
+//   it("test", () => {
+// Test.assertEquals(solution('abcde', 'cde'), true)
+// Test.assertEquals(solution('abcde', 'abc'), false)
+// });
+// });
+
+// Solution:
+
+function solution(str, ending){
+    return str.endsWith(ending)
+   
+   }
+
+   console.log(solution('abcde', 'abc'))
+   console.log(solution('abcde', 'abc'))
